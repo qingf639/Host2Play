407d6ccf58ca9dfce82a4657ae44aa1737060f3a
qingf639 <143203227+qingf639@users.noreply.github.com>
2026-08-30 15:37:14 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ except ImportError:
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=60caaca1-b163-4451-91f7-2ed2df36b92f",  
  "https://host2play.gratis/server/renew?i=8de17775-9c5e-42fd-8372-4e78413d2d21",
-"https://host2play.gratis/server/renew?i=c2d13170-c867-4146-99c5-c262d54a7c52"
+"https://host2play.gratis/server/renew?i=c2d13170-c867-4146-99c5-c262d54a7c52",
     # 添加更多链接
 ]
 
